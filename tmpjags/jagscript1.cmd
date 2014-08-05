0faa23a97de4b62e740a557eeaf566375c7d0b11
Steve Fleming <sfleming@vainglory.cns.nyu.edu>
2014-08-05 15:22:32 -0400

    Update modelRecovery.m

---
+++ tmpjags/jagscript1.cmd
@@ -0,0 +1,15 @@
+load dic
+model in "/Users/sfleming/Documents/HMM/Bayes_metad.txt"
+data in jagsdata.R
+compile, nchains(1)
+parameters in jagsinit1.R
+initialize
+update 1000
+monitor set Mratio, thin(1)
+monitor set Mdiff, thin(1)
+monitor set meta_d, thin(1)
+monitor set cS1, thin(1)
+monitor set cS2, thin(1)
+monitor deviance
+update 10000
+coda *, stem('CODA1')
---
+++ tmpjags/jagsinit2.R
@@ -0,0 +1,8 @@
+"meta.d" <-
+2.20117
+"cS1.raw" <-
+c(-1, -0.6, -0.2, 0.2)
+"cS2.raw" <-
+c(0.2, 0.466667, 0.733333, 1)
+".RNG.name" <- "base::Mersenne-Twister"
+".RNG.seed" <- 2
