fd4ece43653934096cf5e360c95634850960f141
Steve Fleming <sfleming@katebush.fil.ion.ucl.ac.uk>
2018-11-23 12:36:38 +0000

    Added development code for regression version of model

---
+++ exampleFit_group_regression.m
@@ -0,0 +1,49 @@
+% Example script for estimating a regression model
+%
+% Steve Fleming 2018
+
+clear all
+
+Ntrials = 100;
+Nsub = 50;
+c = 0;
+c1 = [-1.5 -1 -0.5];
+c2 = [0.5 1 1.5];
+
+group_d = 2;
+group_baseline_mratio = 0.8;
+sigma = 0.5;
+sigma_beta = 0.2;
+gen_beta = 0.5;
+cov = rand(Nsub,1);
+
+mcmc_params = fit_meta_d_params;
+mcmc_params.estimate_dprime = 0;
+
+for i = 1:Nsub
+        
+        % Generate dprime
+        d(i) = normrnd(group_d, sigma);
+        beta(i) = normrnd(gen_beta, sigma_beta);
+        metad(i) = (group_baseline_mratio + beta(i).*cov(i)).*d(i);    % note this is assuming no unmodelled variance in beta; previous line would do this
+        
+        % Generate data
+        sim = metad_sim(d(i), metad(i), c, c1, c2, Ntrials);
+        
+        nR_S1{i} = sim.nR_S1;
+        nR_S2{i} = sim.nR_S2;
+        
+end
+
+%% Regression fit
+% fit = fit_meta_d_mcmc_group(nR_S1, nR_S2, mcmc_params);
+fit = fit_meta_d_mcmc_regression(nR_S2, nR_S1, cov, mcmc_params);
+
+% % Call plotSamples to plot posterior of group Mratio
+plotSamples(exp(fit.mcmc.samples.mu_logMratio))
+hdi = calc_HDI(exp(fit.mcmc.samples.mu_logMratio(:)));
+fprintf(['\n HDI on meta-d/d: ', num2str(hdi) '\n\n'])
+
+plotSamples(fit.mcmc.samples.mu_beta1)
+hdi = calc_HDI(fit.mcmc.samples.mu_beta1(:));
+fprintf(['\n HDI on beta1: ', num2str(hdi) '\n\n'])
---
+++ fit_meta_d_mcmc_regression.m
@@ -0,0 +1,182 @@
+function fit = fit_meta_d_mcmc_regression(nR_S1, nR_S2, cov, mcmc_params, fncdf, fninv)
+% HMeta-d for between-subjects regression on meta-d'/d'
+% cov is a s x n matrix of covariates, where s=number of subjects, n=number
+% of covariates 
+% See fit_meta_d_mcmc_group for full details
+%
+% Steve Fleming 2017
+
+cwd = pwd;
+
+if ~exist('fncdf','var') || isempty(fncdf)
+    fncdf = @normcdf;
+end
+
+if ~exist('fninv','var') || isempty(fninv)
+    fninv = @norminv;
+end
+
+Nsubj = length(nR_S1);
+nRatings = length(nR_S1{1})/2;
+
+for n = 1:Nsubj
+    if length(nR_S1{n}) ~= nRatings*2 || length(nR_S2{n}) ~= nRatings*2
+        error('Subjects do not have equal numbers of response categories');
+    end
+    % Get type 1 SDT parameter values
+    counts(n,:) = [nR_S1{n} nR_S2{n}];
+    nTot(n) = sum(counts(n,:));
+    % Adjust to ensure non-zero counts for type 1 d' point estimate (not
+    % necessary if estimating d' inside JAGS)
+    adj_f = 1/length(nR_S1{n});
+    nR_S1_adj = nR_S1{n} + adj_f;
+    nR_S2_adj = nR_S2{n} + adj_f;
+    
+    ratingHR  = [];
+    ratingFAR = [];
+    for c = 2:nRatings*2
+        ratingHR(end+1) = sum(nR_S2_adj(c:end)) / sum(nR_S2_adj);
+        ratingFAR(end+1) = sum(nR_S1_adj(c:end)) / sum(nR_S1_adj);
+    end
+    
+    t1_index = nRatings;
+    
+    d1(n) = fninv(ratingHR(t1_index)) - fninv(ratingFAR(t1_index));
+    c1(n) = -0.5 .* (fninv(ratingHR(t1_index)) + fninv(ratingFAR(t1_index)));
+end
+
+%% Sampling
+if ~exist('mcmc_params','var') || isempty(mcmc_params)
+    % MCMC Parameters
+    mcmc_params.response_conditional = 0;   % response-conditional meta-d?
+    mcmc_params.estimate_dprime = 0;    % also estimate dprime in same model?
+    mcmc_params.nchains = 3; % How Many Chains?
+    mcmc_params.nburnin = 1000; % How Many Burn-in Samples?
+    mcmc_params.nsamples = 10000;  %How Many Recorded Samples?
+    mcmc_params.nthin = 1; % How Often is a Sample Recorded?
+    mcmc_params.doparallel = 0; % Parallel Option
+    mcmc_params.dic = 1;
+end
+% Ensure init0 is correct size
+if ~isfield(mcmc_params, 'init0')
+    for i=1:mcmc_params.nchains
+        mcmc_params.init0(i) = struct;
+    end
+end
+
+datastruct = struct('d1', d1, 'c1', c1,'nsubj',Nsubj,'counts', counts,'cov', cov, 'nratings', nRatings, 'nTot', nTot, 'Tol', 1e-05);
+
+% Select model file and parameters to monitor
+
+model_file = 'Bayes_metad_group_regress_nodp.txt';
+monitorparams = {'d1', 'c1', 'mu_logMratio','sigma_logMratio','mu_c2','sigma_c2','mu_beta1', 'sigma_beta1', 'Mratio', 'cS1','cS2', 'lambda_logMratio'};
+
+% Use JAGS to Sample
+tic
+fprintf( 'Running JAGS ...\n' );
+[samples, stats] = matjags( ...
+    datastruct, ...
+    fullfile(pwd, model_file), ...
+    mcmc_params.init0, ...
+    'doparallel' , mcmc_params.doparallel, ...
+    'nchains', mcmc_params.nchains,...
+    'nburnin', mcmc_params.nburnin,...
+    'nsamples', mcmc_params.nsamples, ...
+    'thin', mcmc_params.nthin, ...
+    'dic', mcmc_params.dic,...
+    'monitorparams', monitorparams, ...
+    'savejagsoutput' , 0 , ...
+    'verbosity' , 1 , ...
+    'cleanup' , 1 , ...
+    'workingdir' , 'tmpjags' );
+toc
+
+% Package group-level output
+if isrow(stats.mean.cS1)
+    stats.mean.cS1 = stats.mean.cS1';
+    stats.mean.cS2 = stats.mean.cS2';
+end
+fit.t2ca_rS1  = stats.mean.cS1;
+fit.t2ca_rS2  = stats.mean.cS2;
+fit.d1 = stats.mean.d1;
+fit.c1 = stats.mean.c1;
+
+fit.mu_logMratio = stats.mean.mu_logMratio;
+fit.sigma_logMratio = stats.mean.sigma_logMratio;
+fit.mu_beta1 = stats.mean.mu_beta1;
+fit.sigma_beta1 = stats.mean.sigma_beta1;
+fit.Mratio = stats.mean.Mratio;
+fit.meta_d   = fit.Mratio.*stats.mean.d1;
+
+fit.mcmc.dic = stats.dic;
+fit.mcmc.Rhat = stats.Rhat;
+fit.mcmc.samples = samples;
+fit.mcmc.params = mcmc_params;
+
+for n = 1:Nsubj
+    
+    
+    %% Data is fit, now package output
+    I_nR_rS2 = nR_S1{n}(nRatings+1:end);
+    I_nR_rS1 = nR_S2{n}(nRatings:-1:1);
+    
+    C_nR_rS2 = nR_S2{n}(nRatings+1:end);
+    C_nR_rS1 = nR_S1{n}(nRatings:-1:1);
+    
+    for i = 2:nRatings
+        obs_FAR2_rS2(i-1) = sum( I_nR_rS2(i:end) ) / sum(I_nR_rS2);
+        obs_HR2_rS2(i-1)  = sum( C_nR_rS2(i:end) ) / sum(C_nR_rS2);
+        
+        obs_FAR2_rS1(i-1) = sum( I_nR_rS1(i:end) ) / sum(I_nR_rS1);
+        obs_HR2_rS1(i-1)  = sum( C_nR_rS1(i:end) ) / sum(C_nR_rS1);
+    end
+    
+    
+    % Calculate fits based on either vanilla or response-conditional model
+    s = 1;
+    %% find estimated t2FAR and t2HR
+    meta_d = fit.meta_d(n);
+    S1mu = -meta_d/2; S1sd = 1;
+    S2mu =  meta_d/2; S2sd = S1sd/s;
+    
+    C_area_rS2 = 1-fncdf(fit.c1(n),S2mu,S2sd);
+    I_area_rS2 = 1-fncdf(fit.c1(n),S1mu,S1sd);
+    
+    C_area_rS1 = fncdf(fit.c1(n),S1mu,S1sd);
+    I_area_rS1 = fncdf(fit.c1(n),S2mu,S2sd);
+    
+    t2c1 = [fit.t2ca_rS1(n,:) fit.t2ca_rS2(n,:)];
+    
+    for i=1:nRatings-1
+        
+        t2c1_lower = t2c1(nRatings-i);
+        t2c1_upper = t2c1(nRatings-1+i);
+        
+        I_FAR_area_rS2 = 1-fncdf(t2c1_upper,S1mu,S1sd);
+        C_HR_area_rS2  = 1-fncdf(t2c1_upper,S2mu,S2sd);
+        
+        I_FAR_area_rS1 = fncdf(t2c1_lower,S2mu,S2sd);
+        C_HR_area_rS1  = fncdf(t2c1_lower,S1mu,S1sd);
+        
+        
+        est_FAR2_rS2(i) = I_FAR_area_rS2 / I_area_rS2;
+        est_HR2_rS2(i)  = C_HR_area_rS2 / C_area_rS2;
+        
+        est_FAR2_rS1(i) = I_FAR_area_rS1 / I_area_rS1;
+        est_HR2_rS1(i)  = C_HR_area_rS1 / C_area_rS1;
+        
+    end
+    
+    fit.est_HR2_rS1(n,:)  = est_HR2_rS1;
+    fit.obs_HR2_rS1(n,:)  = obs_HR2_rS1;
+    
+    fit.est_FAR2_rS1(n,:) = est_FAR2_rS1;
+    fit.obs_FAR2_rS1(n,:) = obs_FAR2_rS1;
+    
+    fit.est_HR2_rS2(n,:)  = est_HR2_rS2;
+    fit.obs_HR2_rS2(n,:)  = obs_HR2_rS2;
+    
+    fit.est_FAR2_rS2(n,:) = est_FAR2_rS2;
+    fit.obs_FAR2_rS2(n,:) = obs_FAR2_rS2;
+end
+cd(cwd);
